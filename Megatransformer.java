3d3529a2125f639e2d31006776089d098bf64ef5
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-09-30 13:49:34 -0600

    Create Megatransformer.java

---
+++ Megatransformer.java
@@ -0,0 +1,5 @@
+public class Megatransformer extends Maximal implements Predaconi{
+    //El megatransformer solo puede formarse entre Maximal y Predacon
+    //Por ello se hace uso de una herencia multiple a partir de una interfaz y una clase
+    public void ataquesEspeciales(){}; //Metodo para los ataques especiales compartidos
+}
