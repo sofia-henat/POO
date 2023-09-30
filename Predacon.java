e381e650df19a9360694b358e99a7550803e6cdd
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-09-30 12:18:44 -0600

    Update Predacon.java

--- Predacon.java
+++ Predacon.java
@@ -1,2 +1,9 @@
 public class Predacon extends Transformer{ //Raza
+    int Taladro = 6; //Unidades de ataque
+
+    @Override
+    void ataquesEspeciales() {  
+    }
+
+    public Predacon(){} //Metodo constructor de la clase hija
 }
