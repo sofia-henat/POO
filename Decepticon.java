267e68478e41093c54bcfdafc3ccabce40d14541
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-09-30 12:17:46 -0600

    Update Decepticon.java

--- Decepticon.java
+++ Decepticon.java
@@ -1,2 +1,10 @@
 public class Decepticon extends Transformer{ //Raza
+    int CañonFusion = 6; //Unidades de ataque
+    int EspadaMegatron = 6; // 6 unidades de ataque
+
+    @Override
+    void ataquesEspeciales(){        
+    }
+    
+    public Decepticon(){} //Metodo constructor de la clase hija
 }
