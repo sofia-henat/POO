07d24a7107c1ecb1abe693a4a29b8d4b106679e8
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-09-30 11:41:30 -0600

    Create Transformer.java

---
+++ Transformer.java
@@ -0,0 +1,16 @@
+abstract class Transformer{
+    final String mundOrigen = "Cybertron";//Mundo de origen
+    String nombre; //Nombre
+    int resistencia; //Resistencia
+
+    //////////Ataques Basicos////////
+    int golpeAlto = 2; //Golpe alto
+    int patada = 3; //Patada
+
+    public class Predacon{
+        
+    }
+
+    //Faltan los ataques especiales
+    abstract void ataquesEspeciales();
+}
