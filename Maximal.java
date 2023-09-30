6e0cc976cd3c74a57139d6c16975e1619f8afcfa
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-09-30 12:18:19 -0600

    Update Maximal.java

--- Maximal.java
+++ Maximal.java
@@ -1,2 +1,11 @@
 public class Maximal extends Transformer{
+    
+    int ArcoFlechas = 5; //Unidades de ataque
+    int BlasterIones = 8; //Unidades de ataque
+
+    @Override
+    void ataquesEspeciales(){
+    }
+
+    public Maximal(){} //Metodo constructor de la clase hija
 }
