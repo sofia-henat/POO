341aaeb55c07c5600c683b00e08b28c0bb9eea07
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-09-30 12:37:06 -0600

    Update Autobot.java

--- Autobot.java
+++ Autobot.java
@@ -1,13 +1,14 @@
 public class Autobot extends Transformer{ //Raza
-    //this.resistencia = 1200; // Unidades
     
     int EspadaPrime = 8; //Unidades de ataque
     int HachaEnergón = 10; //Unidades de ataque
     
-    public Autobot(){} //Metodo constructor de la clase hija
-
     @Override
     void ataquesEspeciales(){
-
     }
+
+    public Autobot(){ //Metodo constructor de la clase hija
+        this.resistencia = 1200; //Se inicializa la resistencia
+        this.nombre = "Optimus Prime";
+    } 
 }
