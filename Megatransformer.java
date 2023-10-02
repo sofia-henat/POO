c5dd32f109a936b4b6de3ab06947dda807d8e168
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-10-02 00:10:10 -0600

    Update Megatransformer.java

--- Megatransformer.java
+++ Megatransformer.java
@@ -1,5 +1,10 @@
 public class Megatransformer extends Maximal implements Predaconi{
     //El megatransformer solo puede formarse entre Maximal y Predacon
     //Por ello se hace uso de una herencia multiple a partir de una interfaz y una clase
-    public void ataquesEspeciales(){}; //Metodo para los ataques especiales compartidos
+    private int resistencia = 0;
+
+    public int ataquesEspeciales(){
+        return resistencia;
+    } //Metodo para los ataques especiales compartidos
+  
 }
