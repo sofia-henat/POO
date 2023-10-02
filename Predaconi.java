707972a07650e5f43932eae19054e39291ce8f34
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-10-01 20:43:42 -0600

    Update Predaconi.java

--- Predaconi.java
+++ Predaconi.java
@@ -1,6 +1,6 @@
 public interface Predaconi{ 
     //Predacon tiene una interfaz para permitirnos hacer herencia multiple
-    public void ataquesEspeciales();
+    public int ataquesEspeciales();
     
     //En la interfaz Predaconi se debe de poner los metodos que se usan en Predacon
 }
