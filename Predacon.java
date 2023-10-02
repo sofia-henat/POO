8b738874910971174befc6ecd4e4b818277bde02
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-10-01 20:43:17 -0600

    Update Predacon.java

--- Predacon.java
+++ Predacon.java
@@ -2,7 +2,10 @@ public class Predacon extends Transformer implements Predaconi{ //Raza
     int Taladro = 6; //Unidades de ataque
 
     @Override
-    public void ataquesEspeciales(){  
+    public int ataquesEspeciales(){
+        System.out.println("Se gano un ataque especial");
+        int combo = (golpeAlto * 2) + patada; //Daño de los tres golpes basicos
+        return combo;
     }
 
     public Predacon(){ //Metodo constructor de la clase hija
