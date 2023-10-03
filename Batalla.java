05c1fad911434ff14ace6de26e9af2fc1d8411ca
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-10-02 18:59:45 -0600

    Update Batalla.java

--- Batalla.java
+++ Batalla.java
@@ -4,6 +4,20 @@ public class Batalla{
     public static void main(String[] args){
 
         //Se crean los transformers que iran en ambos bandos
+
+        //Lideres bando 1
+        System.out.println("||||| Autobot Lider bando 1 |||||");
+        Autobot aLider = new Autobot("Lider");
+        System.out.println("||||| Decepticon Lider bando 1 |||||");
+        Decepticon dLider = new Decepticon("Lider");
+
+        //Lideres bando 2
+        System.out.println("||||| Maximal Lider bando 2 |||||");
+        Maximal mLider = new Maximal("Lider");
+        System.out.println("||||| Decepticon Lider bando 2 |||||");
+        Predacon pLider = new Predacon("Lider");
+
+        //Comunes
         System.out.println("||||| Autobot bando 1 |||||");
         Autobot a1 = new Autobot();
         System.out.println("||||| Autobot bando 2 |||||");
@@ -23,12 +37,16 @@ public class Batalla{
 
         //Los Array List nos serviran para guardar los objetos de tipo Transformer en cada uno de los bandos
         ArrayList<Transformer> Bando1 = new ArrayList<>();
+        Bando1.add(aLider);
+        Bando1.add(dLider);
         Bando1.add(a1);
         Bando1.add(d1);
         Bando1.add(m1);
         Bando1.add(p1);
 
         ArrayList<Transformer> Bando2 = new ArrayList<>();
+        Bando1.add(mLider);
+        Bando1.add(pLider);
         Bando2.add(a2);
         Bando2.add(d2);
         Bando2.add(m2);
