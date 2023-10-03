447ddcc329d8498ba720e32331cb7bb1fe108e67
sofia-henat <136744202+sofia-henat@users.noreply.github.com>
2023-10-02 22:58:07 -0600

    Update Decepticon.java

--- Decepticon.java
+++ Decepticon.java
@@ -9,16 +9,18 @@ public class Decepticon extends Transformer{ //Raza
     
     public Decepticon(){ //Metodo constructor de la clase hija
         setResistencia(1200); //Se inicializa la resistencia
+        this.raza = "Decepticon";
         this.nombre = "Megatron";
         //Se escoge un arma en base a su raza
-        Arma arma = new Armas(nombre);
+        Arma arma = new Armas(this.raza);
     } 
 
     public Decepticon(String Lider){
         setResistencia(1200); //Se inicializa la resistencia
         resistenciaInicial = 1200;//Se guarda la resistencia inicial
+        this.raza = "Decepticon";
         this.nombre = "Megatron Lider";
         //Se escoge un arma en base a su raza
-        Arma arma = new Armas(nombre);
+        Arma arma = new Armas(this.raza);
     }
 }
